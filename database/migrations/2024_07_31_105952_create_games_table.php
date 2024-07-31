79dc6bc136d6eb49bad074daf52424161dc5ab77
ebrionesm <168421130+ebrionesm@users.noreply.github.com>
2024-07-31 13:46:49 +0200

    migrations

---
+++ database/migrations/2024_07_31_105952_create_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->integer('dieOne');
+            $table->integer('dieTwo');
+            $table->boolean('victory');
+            $table->unsignedInteger('id_player');
+            $table->timestamps();
+
+            $table->foreign('id_player')->references('id')->on('players')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
